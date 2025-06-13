12928658efdb52c54e8c5f744833347563c48b18
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-13 21:13:59 +0300

    created pizza python file

---
+++ server/models/pizza.py
@@ -0,0 +1,10 @@
+from server import db
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+
+    
