4baaacf00c1ac69ce41930af4b115ad177f2299b
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-13 21:04:07 +0300

    started on first file

---
+++ server/models/restaurant.py
@@ -0,0 +1,10 @@
+from server import db
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String, nullable=False)
+
+    
