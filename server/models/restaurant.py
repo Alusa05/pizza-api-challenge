69558ffbea263afb23cb45eed73d0b4b575a84c9
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-13 21:11:28 +0300

    completed first file

--- server/models/restaurant.py
+++ server/models/restaurant.py
@@ -7,4 +7,10 @@ class Restaurant(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
     address = db.Column(db.String, nullable=False)
 
+    restaurant_pizzas = db.relationship(
+        'RestaurantPizza',
+        backref='restaurant',
+        cascade='all, delete-orphan')
     
+    def __repr__(self):
+        return f'<Restaurant {self.name}>'
