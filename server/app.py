58d58b16be7a523e9901024d92ac03978394df44
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-14 07:35:26 +0300

    completed the app file

--- server/app.py
+++ server/app.py
@@ -13,4 +13,15 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from server.controllers
+    from server.controllers.restaurant_controller import restaurants_bp
+    from server.controllers.pizza_controller import pizzas_bp
+    from server.controllers.restaurant_pizza_controller import restaurant_pizzas_bp
+
+    app.register_blueprint(restaurants_bp)
+    app.register_blueprint(pizzas_bp)
+    app.register_blueprint(restaurant_pizzas_bp)
+
+    return app
+app = create_app()
+
+from server.models.restaurant import restaurant, pizza, restaurant_pizza
