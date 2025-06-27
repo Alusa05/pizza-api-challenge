a36f30995ed7e9a1de6e94c54333e1a382d832cb
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-27 10:46:41 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -1,19 +1,15 @@
-from flask import Flask
+from server import create_app, db
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
+app = create_app()
 migrate = Migrate(app, db)
 
+# Import models after app creation to avoid circular imports
 from server.models.restaurant import Restaurant
 from server.models.pizza import Pizza
 from server.models.restaurant_pizza import RestaurantPizza
 
-# Import controllers
+# Import and register blueprints
 from server.controllers.restaurant_controller import restaurants_bp
 from server.controllers.pizza_controller import pizzas_bp
 from server.controllers.restaurant_pizza_controller import restaurant_pizzas_bp
