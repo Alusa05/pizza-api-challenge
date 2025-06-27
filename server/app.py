f0f85f4673861e57a4a878d47b98376547105ec7
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-27 10:47:41 +0300

    editing some stuff

--- server/app.py
+++ server/app.py
@@ -1,23 +1,6 @@
-from server import create_app, db
-from flask_migrate import Migrate
+from server import create_app
 
 app = create_app()
-migrate = Migrate(app, db)
 
-# Import models after app creation to avoid circular imports
-from server.models.restaurant import Restaurant
-from server.models.pizza import Pizza
-from server.models.restaurant_pizza import RestaurantPizza
-
-# Import and register blueprints
-from server.controllers.restaurant_controller import restaurants_bp
-from server.controllers.pizza_controller import pizzas_bp
-from server.controllers.restaurant_pizza_controller import restaurant_pizzas_bp
-
-app.register_blueprint(restaurants_bp)
-app.register_blueprint(pizzas_bp)
-app.register_blueprint(restaurant_pizzas_bp)
-
-@app.route('/')
-def home():
-    return "Welcome to the Pizza Restaurant API!"
+if __name__ == '__main__':
+    app.run(debug=True)
